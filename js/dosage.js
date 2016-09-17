2f3a02bda64651df24023a062bae2b959d3eb1a0
Kieran <octavian13@shaw.ca>
2016-09-17 15:58:03 -0700

    [JS] Added put commands to update firebase

---
+++ js/dosage.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<div id="id01"></div>
+
+<script>
+var url = "https://lumohacks-med-disp.firebaseio.com/patients/jeffrey_leung/dosage.json";
+var xhr = new XMLHttpRequest();
+xhr.open("PUT", url, true);
+
+
+xhr.send(22);
+</script>
+
+</body>
+</html>
+
