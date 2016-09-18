02932adfd5120f3d1f836d410fcdb3e2fa7e9bba
Jack Chao <euleriepsilon@gmail.com>
2016-09-18 04:49:07 -0700

    finished collapsibles

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
-var dosagePutURL = "https://lumohacks-med-disp.firebaseio.com/patients/jeffrey_leung/dosage.json";
-// var xhr = new XMLHttpRequest();
-// xhr.open("PUT", url, true);
-// xhr.send(22);
+var DOSAGEPUTURL = "https://lumohacks-med-disp.firebaseio.com/patients/jeffrey_leung/dosage.json";
+var ALERTPUT = "https://lumohacks-med-disp.firebaseio.com/patients/jeffrey_leung/alert.json";
 
-var alertPut = "https://lumohacks-med-disp.firebaseio.com/patients/jeffrey_leung/alert.json";
-// var xhr = new XMLHttpRequest();
-// xhr.open("PUT", url, true);
-// xhr.send(true);
+var PATIENTSURL = "https://lumohacks-med-disp.firebaseio.com/patients.json"
 
 
 
@@ -18,22 +13,57 @@ $(document).ready(function() {
 		updateCurrentDosage();
 	})
 
-	$('.dosageDispense').click(function(){
-		$.put({
-			url: dosagePutURL,
-			data: $('#dosageInput').value
-		}).done(function(data){
-			$.put({
-				url: alertPut,
-				data: "true"
+
+  $('expand tr').each(function(){
+      $(this).click(function(){
+          $(this).siblings('tr').slideToggle(300);
+      });
+  });
+	$.get({
+		url: PATIENTSURL
+	}).done(function (data) {
+		console.log("Data: ", data);
+		var patientRow = $('#dummy-patient-row').clone();
+		var hiddenRow = $('#dummy-hidden-row').clone();
+
+		for (i in data){
+
+			hiddenRow.data('patient-data',data[i]);
+			var patientRow = $('#dummy-patient-row').clone();
+			var hiddenRow = $('#dummy-hidden-row').clone();
+			patientRow.attr('href',"#".concat(i));
+			patientRow.attr('aria-controls',i);
+			patientRow.find('.patient-id').text(i)
+			patientRow.find('.patient-name').text(data[i].name)
+			patientRow.find('.patient-nMedication').text(data[i].dosage)
+
+			var correspondingCollapse = hiddenRow.find('.collapse');
+			correspondingCollapse.attr("id", i);
+
+			patientRow.attr('id',"");
+			hiddenRow.attr('id',"");
+			$('#patient_data > tbody').append(patientRow)
+			$('#patient_data > tbody').append(hiddenRow)
+
+		}
+
+		// Done appending; attach click functions
+		$('.dosageDispense').on('click', function(e){
+			e.preventDefault();
+			$.ajax({
+				method: "PUT",
+				url: DOSAGEPUTURL,
+				data: $(this).prevAll('input').val()
 			}).done(function(data){
-				console.log('Dosage Dispended');
+				$.ajax({
+					method: "PUT",
+					url: ALERTPUT,
+					data: "true"
+				}).done(function(data){
+					alert("Dosage Dispensed");
+				})
 			})
 		})
+
 	})
-    $('expand tr').each(function(){
-        $(this).click(function(){
-            $(this).siblings('tr').slideToggle(300);
-        });
-    });
 });
