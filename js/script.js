9fd16b03698634134a3d0168745fa80ae0f025c1
Carlson Chan <carlsonc@sfu.ca>
2016-09-17 22:17:45 -0700

    Fixed dosage number

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
 	function updateCurrentDosage(){
     	document.getElementById('msg').innerText = document.getElementById('dosageInput').value;
 	}
+	$('#dosageInput').click(function(){
+
+		updateCurrentDosage();
+	})
     $('expand tr').each(function(){
         $(this).click(function(){
             $(this).siblings('tr').slideToggle(300);            
