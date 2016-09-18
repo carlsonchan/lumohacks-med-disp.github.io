f2abf1d3dad6572ccb42f42def1d4cf471f43062
Carlson Chan <carlsonc@sfu.ca>
2016-09-17 21:58:00 -0700

    Added JQuery and JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-function updateCurrentDosage(){
-    document.getElementById('msg').innerText = document.getElementById('dosageInput').value;
-}
+$(document).ready(function() {
+	function updateCurrentDosage(){
+    	document.getElementById('msg').innerText = document.getElementById('dosageInput').value;
+	}
+    $('expand tr').each(function(){
+        $(this).click(function(){
+            $(this).siblings('tr').slideToggle(300);            
+        });        
+    });  
+});
+
